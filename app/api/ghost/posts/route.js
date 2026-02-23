88be2c97e515612ecefa157229ffd17c914c76cb
Sergey Bobrovnikov <sergobb@gmail.com>
2026-02-23 12:04:27 +0300

    Remove GHOST_URL

--- app/api/ghost/posts/route.js
+++ app/api/ghost/posts/route.js
@@ -1,4 +1,4 @@
-const GHOST_URL = (process.env.GHOST_URL || "http://213.131.1.29").replace(/\/$/, "");
+const GHOST_URL = (process.env.EXTERNAL_GHOST_URL || "http://213.131.1.29").replace(/\/$/, "");
 const GHOST_KEY = process.env.GHOST_CONTENT_API_KEY || "";
 const GHOST_CLIENT_ID = process.env.GHOST_CLIENT_ID || "ghost-frontend";
 const GHOST_CLIENT_SECRET = process.env.GHOST_CLIENT_SECRET || "";
@@ -7,10 +7,10 @@ const emptyGhostResponse = { posts: [], meta: {} };
 
 function buildGhostUrl() {
   if (GHOST_CLIENT_SECRET) {
-    return `${GHOST_URL}/articles/ghost/api/v0.1/posts/?client_id=${encodeURIComponent(GHOST_CLIENT_ID)}&client_secret=${encodeURIComponent(GHOST_CLIENT_SECRET)}&include=tags&filter=tag:magnetosphere-ru`;
+    return `${GHOST_URL}/ghost/api/v0.1/posts/?client_id=${encodeURIComponent(GHOST_CLIENT_ID)}&client_secret=${encodeURIComponent(GHOST_CLIENT_SECRET)}&include=tags&filter=tag:magnetosphere-ru`;
   }
   if (GHOST_KEY) {
-    return `${GHOST_URL}/articles/ghost/api/v3/content/posts/?key=${encodeURIComponent(GHOST_KEY)}&include=tags&filter=tag:magnetosphere-ru`;
+    return `${GHOST_URL}/ghost/api/v3/content/posts/?key=${encodeURIComponent(GHOST_KEY)}&include=tags&filter=tag:magnetosphere-ru`;
   }
   return null;
 }
